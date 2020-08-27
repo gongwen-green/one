5c9bb8a000e5757e40d2dd4cf2734211536445f0
spuiln <251531091@qq.com>
2020-08-27 15:30:41 +0800

    target

---
+++ src/main/java/caogen_test/two/App.java
@@ -0,0 +1,14 @@
+package caogen_test.two;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("我想敲代码！");
+    }
+}
